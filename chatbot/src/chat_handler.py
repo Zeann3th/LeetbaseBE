260f56af82db00f0c6d6beb484107b00c3475b51
2notprime <long3172004@gmail.com>
2025-03-25 01:14:16 +0700

    Try llama 3.1 with Ollama

--- chatbot/src/chat_handler.py
+++ chatbot/src/chat_handler.py
@@ -1,3 +1,6 @@
+import ollama
+import asyncio
+
 class ChatHandler:
     def __init__(self):
         self.tools = [
@@ -10,3 +13,22 @@ class ChatHandler:
                 },
             }
         ]
+    async def run(self, model: str, query: str):
+        client = ollama.AsyncClient()
+        messages = [{
+            "role": "user",
+            "content": query
+        }]
+        response = await client.chat(
+            model=model,
+            messages=messages
+        )
+        print(response)
+        return response
+
+bot = ChatHandler()
+
+async def main():
+    response = await bot.run("llama3.1", "What is the current weather in New York City?")
+    return response
+print(asyncio.run(main()))
